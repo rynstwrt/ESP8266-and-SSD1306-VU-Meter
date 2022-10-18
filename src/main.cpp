2aee2c5fbb4fcfcc352be8bc7a6ecbabbec7ffce
Ryan Stewart <ryanstewartalex@gmail.com>
2022-10-18 12:21:44 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,10 @@
 #include <Wire.h>
 #include <SPI.h>
 #include <Adafruit_GFX.h>
+#include <Fonts/FreeMonoBold9pt7b.h>
 #include <Adafruit_SSD1306.h>
 #include <bitmap.h>
+
 
 
 /* Definitions */
@@ -22,13 +24,33 @@
 
 #define NEEDLE_CENTER_HORIZ 65  // horizontal center for needle animation
 #define NEEDLE_CENTER_VERT 85  // vertical "center" (out of screen bounds) for needle animation
-#define NEEDLE_ANIM_LENGTH 80  // length of needle animation/arch of needle travel        
+#define NEEDLE_ANIM_LENGTH 80 //80  // length of needle animation/arch of needle travel        
+
 
 
 /* Variables */
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 unsigned int audioSample;
-static const float RADIAN = 180 / PI; 
+const float RADIAN = 180 / PI; 
+const GFXfont* FONT = &FreeMonoBold9pt7b;
+
+
+
+/* Helper function to display horizontally centered text */
+void horizCenterPrintln(String text)
+{   
+    int16_t x1, y1;
+    uint16_t w, h;
+    display.getTextBounds(text, 0, 0, &x1, &y1, &w, &h);
+
+    int horizOffset = round(SCREEN_WIDTH / 2.0 - w / 2.0);
+    static int horizCenterPrintlnYOffset = 0; // Keeps value b/c of how static vars in a function work in C++.
+    horizCenterPrintlnYOffset += h;
+
+    display.setCursor(horizOffset, horizCenterPrintlnYOffset);
+    display.print(text);
+}
+
 
 
 /* Setup function */
@@ -45,7 +67,19 @@ void setup()
         Serial.println("FAILED TO CONNECT TO OLED!");
     else
         Serial.println("OLED CONNECTED!");
+
+    display.clearDisplay();
+    display.setFont(FONT);
+    display.setTextSize(1);
+    display.setTextColor(SCREEN_COLOR);
+
+    horizCenterPrintln("FOR DJ <3");
+    horizCenterPrintln("-Ryn");
+
+    display.display();
+    delay(2500);
 }
+
 
 
 /* Loop function */
@@ -55,7 +89,6 @@ void loop()
     unsigned int peakToPeakAmp = 0;
     unsigned int signalMax = AUDIO_ANALOG_MIN;
     unsigned int signalMin = AUDIO_ANALOG_MAX;
-
 
     while (millis() - startMS < VU_SAMPLE_WINDOW)
     {
@@ -70,7 +103,6 @@ void loop()
             signalMin = audioSample;
     }
 
-
     peakToPeakAmp = signalMax - signalMin; 
     
     float meterValue = peakToPeakAmp * 330 / 1024;  // convert volts to arrow information...
@@ -78,16 +110,6 @@ void loop()
 
     int needleXCoord = (NEEDLE_CENTER_HORIZ + (sin(meterValue / RADIAN) * NEEDLE_ANIM_LENGTH));
     int needleYCoord = (NEEDLE_CENTER_VERT - (cos(meterValue / RADIAN) * NEEDLE_ANIM_LENGTH));
-
-
-    // Serial.println("----------------------");
-    // Serial.print("sample: ");
-    // Serial.println(audioSample);
-    // Serial.println("p2p: ");
-    // Serial.println(peakToPeakAmp);
-    // Serial.println("metervalue: ");
-    // Serial.println(meterValue);
-
 
     display.clearDisplay();
     display.drawBitmap(0, 0, VU_METER_BG_BITMAP, SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_COLOR);
